65280df34738e93cbe46ebeb6134f1ac51f1be3d
Mrunmayi-06 <mrunmayis68@gmail.com>
2025-03-29 21:36:54 +0530

    Add files via upload

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,5 @@
+a=int(input())#takes first number as input
+b=int(input())#takes second number as input
+print(a+b)#prints sum of two numbers
+print(a-b)#prints difference of two numbers(first - second).
+print(a*b)#prints product of the two numbers.
